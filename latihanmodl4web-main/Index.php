289d157c365e7ac1d1aa0f57b23b94f97ae5f8ec
taqimanjay <151718265+taqimanjay@users.noreply.github.com>
2023-11-22 14:16:41 +0700

    Add files via upload

---
+++ latihanmodl4web-main/Index.php
@@ -0,0 +1,10 @@
+<?php
+include "Controller/ProductController.php";
+
+use Controller\ProductController;
+
+// Deklarasikan objek class
+$productController = new ProductController;
+
+// Tampilkan hasil kembalian dari method getAll Product menggunakan echo 
+echo $productController -> getAllProduct();
